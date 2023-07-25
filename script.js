dd7d8cbc0161e631b90d124969fde2e1aca8d231
Ralph Andy Menz <andyralphmenz@gmail.com>
2023-07-25 22:34:16 +0000

    js in progress

--- script.js
+++ script.js
@@ -1,57 +1,32 @@
-userChoice = 0
-computerChoice = 0
+const rock = document.querySelector("#rock")
+const paper = document.querySelector("#paper")
+const scissors = document.querySelector("#scissors")
 
-// rock = document.querySelector("#rock")
-// paper = document.querySelector("#paper")
-// scissors = document.querySelector("#scissors")
-
-
-// const options = document.querySelector(".option")
-
-options = { rock , paper , scissors}
-
+elements = [ rock , paper, scissors]
 
 // rock = '1'
 // paper = '2'
 // scissors = '3'
 
-
-
-
-
-options.forEach((option) => option.addEventListener("click",(event) => {random(event)}))
-   
+elements.forEach((el) => el.addEventListener("click", event => random(event)))
 function random(event){
 
-        // console.log(event.target.innerHTML);
-
-    const num =  Math.floor((Math.random() * options.length))
-    text = event.target.className
- 
+   text = event.target.id
+   const num =  Math.floor((Math.random() * elements.length + 1))
+   
 
     if (num == 0){
-        if(text == 1){console.log('tie')}
-        if(text == 2){console.log('win')}
-        if(text == 3){console.log('lose')}
+        if(text == "rock"){console.log('tie')}
+        if(text == "paper"){console.log('win')}
+        if(text == "scissors"){console.log('lose')}
     }
-    if (num == 1){
-        if(text == 1){console.log('lose')}
-        if(text == 2){console.log('tie')}
-        if(text == 3){console.log('win')}
+    else if (num == 1){
+        if(text == "rock"){console.log('lose')}
+        if(text == "paper"){console.log('tie')}
+        if(text == "scissors"){console.log('win')}
     }
-    if (num == 2){
-        if(text == 1){console.log('win')}
-        if(text == 2){console.log('lose')}
-        if(text == 3){console.log('tie')}
-    }
-
-
-
-    console.log(num)
-}
-    
-
-
-
-
-
+    else if (num == 2){
+        if(text == "rock"){console.log('win')}
+        if(text == "paper"){console.log('lose')}
+        if(text == "scissors"){console.log('tie')}
+    }}
